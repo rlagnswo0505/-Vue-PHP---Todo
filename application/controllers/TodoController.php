b1e38a6f209539dac905f1c2dfe60257e1b25336
HoonJae <rlagnswo0505@naver.com>
2022-07-12 16:59:23 +0900

    vue 빌드 후 php 폴더에 합치기

--- application/controllers/TodoController.php
+++ application/controllers/TodoController.php
@@ -2,22 +2,26 @@
 namespace application\controllers;
 
 class TodoController extends Controller {
+  public function main() {
+    return "index.html";
+  }
 public function index() {
   switch(getMethod()){
     case _GET:   
-    return $this->model->selTodoList($param);
+    return $this->model->selTodoList();
     case _POST:
       $json = getJson();
       return [_RESULT => $this->model->insTodo($json)];
     case _DELETE:
       $urlPaths = getUrlPaths();
-      $param = ["itodo" => 0];
-      if(isset($urlPaths[2])){
-        $param['itodo'] = intval($urlPaths[2]);
+      $param = [
+        "itodo" => 0  // 디폴트 값은 0
+      ];
+      if(isset($urlPaths[2])) { // itodo값이 0 이면 전체 삭제, 0 이 아니면 부분 삭제
+        $param["itodo"] = intval($urlPaths[2]); 
       }
       return [_RESULT => $this->model->delTodo($param)];
 
+    }
   }
-}
-
 }
--- application/models/TodoModel.php
+++ application/models/TodoModel.php
@@ -6,24 +6,23 @@ class TodoModel extends Model {
   public function insTodo(&$param){
     $sql = "INSERT INTO t_todo (todo) VALUES (:todo)";
     $stmt = $this->pdo->prepare($sql);
-    $stmt->bindValue(":todo", $param["todo"]);        
+    $stmt->bindValue(":todo", $param['todo']);        
     $stmt->execute();
-    return $stmt->rowCount();
+    return intVal($this->pdo->lastInsertId());
   }
-  public function selTodoList(&$param){
+  public function selTodoList(){
     $sql = "SELECT * FROM t_todo";
     $stmt = $this->pdo->prepare($sql);      
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_OBJ);
   }
-  public function delTodo(&$param){
+  public function delTodo(&$param) {
     $sql = "DELETE FROM t_todo";
-    $itodo = $param['itodo'];
-    if($itodo > 0){
-      $sql .= " WHERE itodo = {$itodo}";
+    $itodo = $param["itodo"];
+    if($itodo > 0) {
+      $sql .= " WHERE itodo={$itodo}";
     }
     $stmt = $this->pdo->prepare($sql);
-    $stmt->bindValue(":itodo", $param["itodo"]);        
     $stmt->execute();
     return $stmt->rowCount();
   }
